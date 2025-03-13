106707ebadef393d6ae447a7d44d5cd29382a7a3
Gustavo <sgkenzo212007@gmail.com>
2025-03-13 11:25:15 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Qual a linha? ");
+		int row = sc.nextInt();
+		System.out.print("Qual a coluna? ");
+		int column = sc.nextInt();
+		Position position = new Position(row, column);
 
+		System.out.print("Posicao: "+position);
 	}
 
 }
